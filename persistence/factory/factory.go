2d4740d63e1e1b8945d31178d47e0c456fecd8a1
Sascha Andres <sascha.andres@outlook.com>
2017-08-02 08:10:17 +0200

    Added toggl driver

--- persistence/factory/factory.go
+++ persistence/factory/factory.go
@@ -1,15 +1,21 @@
 package factory
 
 import (
+	"fmt"
+
 	"github.com/pkg/errors"
 	"github.com/sascha-andres/timenote/persistence"
 	"github.com/sascha-andres/timenote/persistence/mysqldriver"
+	"github.com/sascha-andres/timenote/persistence/toggldriver"
 )
 
 // CreatePersistence returns the selected backend
 func CreatePersistence(driver, dsn string) (persistence.Persistor, error) {
-	if driver != "mysql" {
-		return nil, errors.New("Only a MySQL driver exists")
+	if driver == "mysql" {
+		return mysqldriver.NewMysql(dsn)
 	}
-	return mysqldriver.NewMysql(dsn)
+	if driver == "toggl" {
+		return toggldriver.NewToggl(dsn)
+	}
+	return nil, errors.New(fmt.Sprintf("Driver %s does not exist", driver))
 }
---
+++ persistence/toggldriver/toggl.go
@@ -0,0 +1,118 @@
+package toggldriver
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+
+	"github.com/jason0x43/go-toggl"
+	"github.com/sascha-andres/timenote"
+	"github.com/sascha-andres/timenote/persistence"
+)
+
+type (
+	TogglPersistor struct {
+		dsn     string
+		session toggl.Session
+	}
+)
+
+func NewToggl(dsn string) (persistence.Persistor, error) {
+	var res TogglPersistor
+	res.dsn = dsn
+	res.session = toggl.OpenSession(dsn)
+	return &res, nil
+}
+
+func (t *TogglPersistor) New() error {
+	_, err := t.session.StartTimeEntry("")
+	if err != nil {
+		return errors.Wrap(err, "Unable to start a new entry")
+	}
+	return nil
+}
+
+func (t *TogglPersistor) Append(line string) error {
+	account, err := t.session.GetAccount()
+	if err != nil {
+		return errors.Wrap(err, "Unable to get account")
+	}
+	te, err := getCurrentTimeEntry(account)
+	if err != nil {
+		return errors.Wrap(err, "Unable to get running time entry from toggl")
+	}
+	te.Description = line
+	_, err = t.session.UpdateTimeEntry(*te)
+	if err != nil {
+		return errors.Wrap(err, "Unable to update time entry in toggl")
+	}
+	return nil
+}
+
+func (t *TogglPersistor) Tag(name string) error {
+	account, err := t.session.GetAccount()
+	if err != nil {
+		return errors.Wrap(err, "Unable to get account")
+	}
+	te, err := getCurrentTimeEntry(account)
+	if err != nil {
+		return errors.Wrap(err, "Unable to get running time entry from toggl")
+	}
+	if te.HasTag(name) {
+		te.RemoveTag(name)
+	} else {
+		te.AddTag(name)
+	}
+	t.session.UpdateTimeEntry(*te)
+	if err != nil {
+		return errors.Wrap(err, "Unable to update time entry in toggl")
+	}
+	return nil
+}
+
+func (t *TogglPersistor) Done() error {
+	account, err := t.session.GetAccount()
+	if err != nil {
+		return errors.Wrap(err, "Unable to get account")
+	}
+	te, err := getCurrentTimeEntry(account)
+	if err != nil {
+		return errors.Wrap(err, "Unable to get running time entry from toggl")
+	}
+	_, err = t.session.StopTimeEntry(*te)
+	if err != nil {
+		return errors.Wrap(err, "Unable to stop running time entry in toggl")
+	}
+	return nil
+}
+
+func (t *TogglPersistor) Close() error {
+	return nil
+}
+
+func (t *TogglPersistor) ListForDay(delta int) ([]timenote.TimeEntry, error) {
+	return nil, errors.New("Not yet implemented")
+}
+
+func (t *TogglPersistor) Current() (*timenote.TimeEntry, error) {
+	account, err := t.session.GetAccount()
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to get account")
+	}
+	te, err := getCurrentTimeEntry(account)
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to get running time entry from toggl")
+	}
+	var res timenote.TimeEntry
+	res.Note = te.Description
+	return nil, nil
+}
+
+func getCurrentTimeEntry(account toggl.Account) (*toggl.TimeEntry, error) {
+	for _, te := range account.Data.TimeEntries {
+		if nil == te.Stop {
+			return &te, nil
+		}
+	}
+	return nil, fmt.Errorf("No current time entry")
+}
