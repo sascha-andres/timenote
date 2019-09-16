042217871550ee1b261df526fd6b9a78ad944b4a
Sascha Andres <sascha.andres@outlook.com>
2019-09-16 11:04:11 +0200

    feat: add description command

---
+++ timenote/cmd/timestamp_description.go
@@ -0,0 +1,53 @@
+// Copyright © 2018 Sascha Andres <sascha.andres@outlook.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"livingit.de/code/timenote/persistence/factory"
+)
+
+// timestampCurrentCmd represents the current command
+var timestampDescriptionCmd = &cobra.Command{
+	Use:   "description",
+	Short: "Print current timestamp description",
+	Long:  `Prints the current timestamp's description`,
+	Run: func(cmd *cobra.Command, args []string) {
+		persistence, err := factory.CreatePersistence(viper.GetString("persistor"), viper.GetString("dsn"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer func() {
+			err := persistence.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		ts, err := persistence.Current()
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		fmt.Println(ts.Note)
+	},
+}
+
+func init() {
+	timestampCmd.AddCommand(timestampDescriptionCmd)
+}
