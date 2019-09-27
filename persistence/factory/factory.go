0406a4cb2c8d252342d1b79d03e7c107ad54aa76
Sascha Andres <sascha.andres@outlook.com>
2019-09-27 20:55:51 +0200

    feat: remove MySQL driver

--- persistence/factory/factory.go
+++ persistence/factory/factory.go
@@ -5,15 +5,11 @@ import (
 
 	"github.com/pkg/errors"
 	"livingit.de/code/timenote/persistence"
-	"livingit.de/code/timenote/persistence/mysqldriver"
 	"livingit.de/code/timenote/persistence/toggldriver"
 )
 
 // CreatePersistence returns the selected backend
 func CreatePersistence(driver, dsn string) (persistence.Persistor, error) {
-	if driver == "mysql" {
-		return mysqldriver.NewMysql(dsn)
-	}
 	if driver == "toggl" {
 		return toggldriver.NewToggl(dsn)
 	}
