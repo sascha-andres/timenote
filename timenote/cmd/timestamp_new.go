1802ced5dcff11dece69af22ee26850227088be4
Sascha Andres <sascha.andres@outlook.com>
2021-05-30 10:12:22 +0200

    feat: add license info to files
    updated year for changed files

--- timenote/cmd/timestamp_new.go
+++ timenote/cmd/timestamp_new.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 Sascha Andres <sascha.andres@outlook.com>
+// Copyright © 2021 Sascha Andres <sascha.andres@outlook.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
